9d160b4c457d57e5521697d19e6d444cf0f8074c
AllunDev <allun.development@gmail.com>
2024-03-08 16:21:06 +0100

    made changes >:)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Gynoid
 A NSFW booru bot for discord
+
+## Planned features
+Hybrid commands
+Separate default tags and user inputed tags
+Configure default tags with commands
+Add support weaping a tag containing spaces with ""
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
-import base64
-
-import requests
 import json
+import base64
 import discord
+import requests
 
-from discord.ext import commands
-from discord import Interaction
 from dotenv import load_dotenv
+from discord import Interaction
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -30,13 +29,7 @@ async def gelbooru(interaction : Interaction, tags:str = 'none'):
 
     defaultTags = ['sort:random']
     tags = getTagList(tags, defaultTags)
-    url = 'https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&tags='
-
-    for tag in tags:
-        url += tag + '+'
-
-    url += '&user_id=' + GELBOORU_USER
-    url += '&api_key=' + GELBOORU_KEY
+    url = f'https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&json=1&user_id={GELBOORU_USER}&api_key={GELBOORU_KEY}&tags=' + '+'.join(tags)
 
     response = requests.get(url)
 
@@ -44,20 +37,28 @@ async def gelbooru(interaction : Interaction, tags:str = 'none'):
         data = json.loads(response.text)
 
         if data['@attributes']['count'] == 0:
-            embed = discord.Embed(color=0xDC322F, description=f'```ansi\n\u001b[0;31m0 \u001b[0mresults found```')
+            missingTags = []
+            for tag in tags:
+                tagResponse = requests.get(f'https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&user_id={GELBOORU_USER}&api_key={GELBOORU_KEY}&name={tag}')
+                tagData = json.loads(tagResponse.text)
+                if tagData['@attributes']['count'] == 0:
+                    missingTags += [tag]
+            if len(missingTags) > 0:
+                embed = discord.Embed(color=0xDC322F, description=f'```ansi\n\u001b[1;31mUnknown tag(s): \u001b[0m{getStringFromTags(missingTags)}```')
+                await interaction.followup.send(embed=embed)
+                return
+
+            embed = discord.Embed(color=0xDC322F, description=f'```ansi\n\u001b[1;31m0 \u001b[0mresults found```').add_field(name='', value=f'```ansi\n\u001b[0;31mTags: \u001b[0m{getStringFromTags(tags)}```')
             await interaction.followup.send(embed=embed)
             return
 
         post = data['post'][0]
-
         url = 'https://gelbooru.com/index.php?page=post&s=view&id=' + str(post['id'])
-        tagList = str(tags).replace('[', '').replace(']', '').replace('\'', '').replace('_', ' ')
-        description = f'```ansi\n\u001b[0;34mTags: \u001b[0m{tagList}```'
+        
+        description = f'```ansi\n\u001b[0;34mTags: \u001b[0m{getStringFromTags(tags)}```'
 
         embed = discord.Embed(title=post['id'], url=url, color=0x00A8FC, description=description).set_image(url=post['file_url'])
-        
         await interaction.followup.send(embed=embed)
-
         return
 
     await interaction.followup.send(f'Error: {response.status_code}')
@@ -99,6 +100,17 @@ async def e621(interaction : Interaction, tags:str = 'none'):
     await interaction.followup.send(f'Error: {response.status_code}')
 
 
+@client.tree.command(name="help", description="list all commands")
+async def help(interaction : Interaction):
+    await interaction.response.defer()
+    
+    embed = discord.Embed(color=0x00A8FC)
+    embed.add_field(name='/gelbooru `tags`', value='get a post from https://gelbooru.com/\n\nexample: **/gelbooru** `cat_ears rating:safe sort:random score:>150`\nwill get a random post with the tag cat ears, rated safe with a score greater than 150\nhttps://gelbooru.com/index.php?page=help&topic=cheatsheet', inline=False)
+    embed.add_field(name='/e621 `tags`', value='get a post from https://e621.net/\n\nexample: **/e621** `fluffy_tail rating:explicit order:score -animated`\nwill get the highest scored post with the fluffy tail tag that is not animated\nhttps://e621.net/help/cheatsheet')
+    
+    await interaction.followup.send(embed=embed)
+
+
 def getTagList(tags, defaultTags):
     if tags == 'none':
         return defaultTags
@@ -113,5 +125,8 @@ def getTagList(tags, defaultTags):
 
     return tags.split()
 
+
+def getStringFromTags(tags):
+    return ', '.join(tags).replace('_', ' ')
 
 client.run(TOKEN)
